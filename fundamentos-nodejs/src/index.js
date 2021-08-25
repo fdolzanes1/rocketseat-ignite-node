aa1ecb8895d6d71c50a1a30b4939a5a5c6c13e45
fdolzanes1 <fdolzanes1@gmail.com>
2021-08-24 21:50:15 -0400

    tipos de parametros

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).send({ message: "hello world!" });
 });
 
-app.get('/courses', (req, res) => {
-  res.status(200).json(["React", "React Native", "Flutter", "NodeJs", "Elixir"]);
+app.get('/courses/:id', (req, res) => {
+  const params = req.params;
+  res.status(200).json([params,"React", "React Native", "Flutter", "NodeJs", "Elixir"]);
+});
+
+app.get('/course', (req, res) => {
+  const query = req.query;
+  res.status(200).json([query,"React", "React Native", "Flutter", "NodeJs", "Elixir"]);
 });
 
 app.post('/courses', (req, res) => {
-  res.status(200).json(["React", "React Native", "Flutter", "NodeJs", "Elixir", "Ruby on Rails"]);
+  res.status(201).json(req.body);
+  //res.status(200).json(["React", "React Native", "Flutter", "NodeJs", "Elixir", "Ruby on Rails"]);
 });
 
 app.patch('/courses', (req, res) => {
