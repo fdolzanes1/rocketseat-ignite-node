efcf541d40c945343d94744c11d03be648a52c80
fdolzanes1 <fdolzanes1@gmail.com>
2022-02-15 17:47:36 -0400

    setting up database

---
+++ masterclass-sqlnode/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5454,
+  username: 'postgres',
+  password: '123456',
+  database: 'masterclass-sqlnode',
+  define: {
+    timestamp: true,
+    underscore: true,
+  },
+};
---
+++ masterclass-sqlnode/src/routes.js
@@ -0,0 +1,8 @@
+const { json } = require('express');
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => { res.json({ message: 'hello world'})});
+
+module.exports = routes;
